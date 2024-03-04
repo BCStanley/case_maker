e3c90c96709044b9fc3b683a9b4cfa946da5a8bb
BCStanley <benstanley0202@gmail.com>
2024-03-04 11:17:37 +0000

    Updating issues.

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# Information about this project
+# Overview 
+
+`case_maker` is a tool for generating, updating, and querying a databse of cases. 
+
+Cases are entered, for ease, in a basic `.xlsx` file. This is a poor solution, but it works well for now, and has the added benefit of being easily edited by multiple people (e.g., using google docs or similar tools.)
+
+The cases are stored in a database in the `.db` format. SQL can be used to consult the database. 
+
+That database can then be searched, and the output it produced as a `.tex` file. 
+
+Shell scripts manage the whole process. 
+
+## Basic Functionality
+
+# Classes 
+
+The following are the list of the main classes within the code. 
+
+# `Case`
+
+The `Case` class is the basic building-block of the whole program, making up the single unit of an individual case entry. It contains the following: 
+1. `name`, which is a `str`, and must be unique.
+   * This is how any case is identified, e.g. "Robinson v Bland", "Re Melbourne"... 
+2. `year`, which is an `int`.
+    * E.g. 1799, 1850... 
+3. `court`, which is a `str`. 
+    * "KB", "Admiralty," "CP" etc. 
+4. `nom_cite` which is a `str` or `None`. 
+    * These are used for nominate citations, e.g. "2 Bing 314". 
+5. `er_cite` which is a `str` or `None`. 
+    * These are used for _English Reports_ citations (e.g. "3 ER 316") _or_; 
+    * Ordinary citations, being the type that take square brackets (e.g. "AC 415"). 
+    * If a case has an AC, KB, etc. citation --- it should be entered as an `er_cite`. 
+6. `comment` which is a `str` or `None`. 
+    * A basic string, giving relevant details or outlines of the case. 
+7. `link` which is a `str` or `None`. 
+   * Typically, this will be a link to `heinonline`, but other platforms and links are available. 
+8. `area_tags` which is a `list` of `str` or `None`.
+   * These provide the general legal or institutional context (e.g. sale, bill of exchange).
+9. `subject_tags` which is a `list` of `str` or `None`.
+   * These provide the relevant subject data, what _legal issues_ are involved. 
+10. `cite_in_tags` which is a `dict` of `{str: str,...}` or `None`.
+   * These authors are usually listed in a form that includes the works involved , e.g. "Story_Ed1" for the First Edition of Story's _Conflict of Laws_. 
+   * The entires are stored as a dictionary, {author: what it is cited for}.  
+11. `authors` which is a `list` of `str` or `None`.
+   * These are the _authors cited by the case_. 
+   * These might be cited by counsel or by the bench,or sometimes by the reporter. 
+12. `special_terms` which is a`list` of `str` or `None`.
+    * Special legal terms, like "_lex loci contractus_" or _privity of contract_ that are used. 
+
+The class also contains the possibility of a `related_cases` entry. This is not used currently. 
+
+### Properties 
+
+### Static Methods 
+
+## `Casebook`
 
--- main.py
+++ main.py
@@ -1,2 +1,116 @@
-print('hello')
+from __future__ import annotations
 
+import re
+from typing import Optional
+from openpyxl.cell import Cell
+
+
+class Case:
+    def __init__(
+            self,
+            name: str,
+            court: str,
+            year: Optional[int] = None,
+            related_cases: Optional[list[str]] = None,
+            nom_cite: Optional[str] = None,
+            er_cite: Optional[str] = None,
+            comment: Optional[str] = None,
+            link: Optional[str] = None,
+            area_tags: Optional[list[str]] = None,
+            subject_tags: Optional[list[str]] = None,
+            cite_in_tags: Optional[dict] = None,
+            authors: Optional[list[str]] = None,
+            special_terms: Optional[list[str]] = None
+    ):
+        self.name = name
+        self.year = year
+        self.link = link
+        self.court = court
+        self.er_cite = er_cite
+        self.nom_cite = nom_cite
+        self.comment = comment
+        self.area_tags = area_tags
+        self.subject_tags = subject_tags
+        self.cite_in_tags = cite_in_tags
+        self.authors = authors
+        self.special_terms = special_terms
+        self.related_cases = related_cases
+
+    def display(self):
+        print(self.display_name)
+
+    @property
+    def display_name(self) -> str:
+        return f'{self.name} ({self.year}) {self.nom_cite}, {self.er_cite}'
+
+    @property
+    def display_area_tags(self) -> str:
+        return ', '.join(self.area_tags)
+
+    @property
+    def display_subject_tags(self) -> str:
+        return ', '.join(self.subject_tags)
+
+    @property
+    def display_author(self) -> str:
+        return ', '.join(self.authors)
+
+    @property
+    def display_special_terms(self) -> str:
+        return ', '.join(self.special_terms)
+
+    @property
+    def display_cite_ins(self) -> str:
+        return '; '.join([f'{person} ({comment})' for person, comment in self.cite_in_tags.items()])
+
+    @staticmethod
+    def from_excel(row: tuple) -> Case:
+        def get_str_value(cell: Cell) -> str | None:
+            return cell.value.strip() if cell.value else None
+
+        def get_year(cell: Cell) -> int | None:
+            if isinstance((date := cell.value), int):
+                return date
+            else:
+                raise Exception(f'Error! Date at row {cell.row} is of type {type(date)}!')
+
+        def get_underscore_list(cell: Cell) -> list[str] | None:
+            if input_str := cell.value:
+                return [item.replace('_', ' ') for item in input_str.split()]
+
+        def get_cite_ins(cell) -> dict | None:
+            def get_items(input_str: str) -> tuple[str, str]:
+                pattern = re.compile(r'^([^[]+)\[([^]]+)\]$')
+                match = pattern.match(input_str)
+                if match:
+                    return match.group(1), match.group(2)
+                else:
+                    print('No match found.')
+
+            if cell.value:
+                input_strings = cell.value.strip().split('; ')
+                out_dict = dict()
+                for input_str in input_strings:
+                    person, comment = get_items(input_str)
+                    out_dict.update({person: comment})
+                return out_dict
+
+        def get_comma_list(cell: Cell) -> list[str] | None:
+            if input_str := cell.value:
+                return input_str.strip().split(', ')
+
+        return Case(
+            name=get_str_value(row[0]),
+            year=get_year(row[1]),
+            nom_cite=get_str_value(row[2]),
+            er_cite=get_str_value(row[3]),
+            court=get_str_value(row[4]),
+            subject_tags=get_underscore_list(row[5]),
+            authors=get_underscore_list(row[6]),
+            related_cases=get_comma_list(row[7]),
+            cite_in_tags=get_cite_ins(row[8]),
+            comment=get_str_value(row[9]),
+            area_tags=get_underscore_list(row[10]),
+            link=get_str_value(row[11]),
+            special_terms=get_comma_list(row[12])
+        )
